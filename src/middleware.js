afcbd0b04b78cfeb7e98a179bc7152aca34aa9fa
oyis12 <oyivosam12@gmail.com>
2026-05-15 15:15:14 +0100

    updated

--- src/middleware.js
+++ src/middleware.js
@@ -8,7 +8,7 @@ export function middleware(request) {
   if (token && (pathname === '/login' || pathname === '/signup')) {
     if (role === 'admin') return NextResponse.redirect(new URL('/admin', request.url));
     if (role === 'business') return NextResponse.redirect(new URL('/dashboard', request.url));
-    return NextResponse.redirect(new URL('/', request.url)); // Default user
+    return NextResponse.redirect(new URL('/', request.url));
   }
 
   if (pathname.startsWith('/dashboard') && role !== 'business' && role !== 'admin') {

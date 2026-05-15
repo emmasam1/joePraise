8a57657e4e85a83a8ee6d217ae30170d8f19d4ac
oyis12 <oyivosam12@gmail.com>
2026-05-15 15:27:46 +0100

    `Removed unused code and updated import statements in verification-successful/page.js`

--- src/app/verification-successful/page.js
+++ src/app/verification-successful/page.js
@@ -1,38 +1,4 @@
 
-// import Link from 'next/link'
-// import React from 'react'
-// import Image from 'next/image'
-
-// const page = () => {
-//   return (
-//     <div className='flex gap-5 justify-center items-center flex-col bg-[#E8FFF7] h-screen w-full font-sans'>
-//       <h1 className="text-4xl font-extrabold text-center text-[#15BE87] tracking-tight">
-//         Email Verification
-//       </h1>
-
-//       <Image 
-//         src="/images/success_icon.png" 
-//         width={200} 
-//         height={200} 
-//         alt="successIcon" 
-//         className='my-5 animate-bounce'
-//         priority
-//       />
-
-//       <p className='text-[#4A4A4A] text-2xl font-bold'>Congratulations</p>
-//       <span className='text-[#4A4A4A] text-lg text-center tracking-wide'>
-//         Your email has been successfully<br/> verified.
-//       </span>
-
-//       <Link href="/dashboard" className='text-[#15BE87] underline font-semibold text-lg hover:text-[#0f9468] transition-colors mt-4'>
-//         Proceed to Dashboard
-//       </Link>
-//     </div>
-//   )
-// }
-
-// export default page;
-
 "use client";
 import Link from 'next/link';
 import React from 'react';
